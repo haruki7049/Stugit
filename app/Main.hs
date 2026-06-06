6deffce28c966ef88d3280646ea097098e95d89b
haruki7049 <tontonkirikiri@gmail.com>
2026-06-06 17:49:12 +0900

    Update

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,24 @@
+{-# LANGUAGE OverloadedStrings, OverloadedLabels, OverloadedRecordDot, ImplicitParams, BlockArguments #-}
+
 module Main (main) where
 
+import GI.Gtk qualified as Gtk
+import GI.Gio qualified as Gio
+import Data.GI.Base (AttrOp ((:=)), new, set, get, on)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  app <- new Gtk.Application [#applicationId := "application-id"]
+  -- `on` によってシグナルが送られたときのコールバックを設定できる
+  -- ここでは app に #activate シグナルが送られたとき
+  _ <- on app #activate do
+    -- callback では ImplicitParams 拡張によってレシーバー `?self` を参照できる
+    -- `get` によってプロパティを参照できる
+    putStrLn $ show (?self `get` #applicationId)
+    win <- new Gtk.ApplicationWindow [#application := ?self]
+    -- `set` によってプロパティを設定できる
+    win `set` [#title := "window title"]
+    win `set` [#defaultWidth := 400, #defaultHeight := 300]
+    -- OverloadedRecordDot 拡張によるメソッド呼び出し風の記法も使える
+    win.present
+  pure ()
