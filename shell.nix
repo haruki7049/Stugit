a2780ae4684250fbb8ef61363ad11072cd5ce566
haruki7049 <tontonkirikiri@gmail.com>
2026-06-06 16:36:28 +0900

    Update

--- shell.nix
+++ shell.nix
@@ -4,7 +4,13 @@
 
 pkgs.mkShell {
   nativeBuildInputs = [
-    pkgs.haskellPackages.ghc
-    pkgs.haskellPackages.cabal-install
+    pkgs.haskellPackages.ghc # GHC haskell compiler
+    pkgs.haskellPackages.cabal-install # Cabal package manager
+    pkgs.pkg-config # pkg-config
+    pkgs.haskellPackages.cabal-fmt # Cabal code formatter
+  ];
+
+  buildInputs = [
+    pkgs.gtk2
   ];
 }
