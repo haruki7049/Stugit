9101f157a13c7bb3d42f3dd367bf73bd36cb9b63
haruki7049 <tontonkirikiri@gmail.com>
2026-06-06 17:36:36 +0900

    Update

--- shell.nix
+++ shell.nix
@@ -12,5 +12,22 @@ pkgs.mkShell {
 
   buildInputs = [
     pkgs.gtk4
+
+    pkgs.gobject-introspection
+    pkgs.libsysprof-capture
+    pkgs.pcre2
+    pkgs.expat
+    pkgs.libxdmcp
+    pkgs.libuuid
+    pkgs.libselinux
+    pkgs.libsepol
+    pkgs.fribidi
+    pkgs.libthai
+    pkgs.libdeflate
+    pkgs.libdatrie
+    pkgs.lerc
+    pkgs.xz
+    pkgs.zstd
+    pkgs.libwebp
   ];
 }
